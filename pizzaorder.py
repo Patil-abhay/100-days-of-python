6b0a6fdfa066e660bc75760ca9118e9429dc5fe9
Abhay patil <103360958+patilabhay0616@users.noreply.github.com>
2022-12-26 15:46:19 +0530

    Update pizzaorder.py

--- pizzaorder.py
+++ pizzaorder.py
@@ -1,3 +1,8 @@
+print("Welcome to python pizza deliveries!! ")
+size = input("What size of pizza do you want? S, M, L\n")
+add_pepperoni = input("Do you want pepperoni?\n")
+extra_cheese = input("Do you want extra chesse?\n")
+
 bill = 0
 if size == "S":
     bill += 15
@@ -7,7 +12,7 @@ else :
     bill += 25
 
 if add_pepperoni == "Y":
-    if size == "S":
+    if size == "S":  
         bill += 2
     else:
         bill += 3
